b411373debe03e729f01a55cce5ff6f8fc256509
lukam <lukam@DESKTOP-4MU60DG>
2021-04-01 09:56:38 +0200

    matagej

--- src/main/java/rs/edu/matgim/zadatak/DB.java
+++ src/main/java/rs/edu/matgim/zadatak/DB.java
@@ -6,16 +6,16 @@ public class DB {
 
     String connectionString = "jdbc:sqlite:src\\main\\java\\Banka.db";
 
-    public void printFilijala() {
+    public void printPositiveRacun()  {
         try ( Connection conn = DriverManager.getConnection(connectionString);  Statement s = conn.createStatement()) {
 
-            ResultSet rs = s.executeQuery("SELECT * FROM Filijala");
+            ResultSet rs = s.executeQuery("SELECT IdRac FROM Racun WHERE Stanje>0");
             while (rs.next()) {
-                int IdFil = rs.getInt("IdFil");
-                String Naziv = rs.getString("Naziv");
-                String Adresa = rs.getString("Adresa");
+                int idRac = rs.getInt(1);
+                
+                
 
-                System.out.println(String.format("%d\t%s\t%s", IdFil, Naziv, Adresa));
+                System.out.println(String.format("%d",idRac ));
             }
 
         } catch (SQLException ex) {
@@ -23,5 +23,59 @@ public class DB {
             System.out.println(ex);
         }
     }
+    
+    boolean nesto(int idRac,float sum) throws SQLException
+            {
+              try ( Connection conn = DriverManager.getConnection(connectionString);  Statement s = conn.createStatement()){
+              ResultSet rs=s.executeQuery("SELECT Stanje , DozvMinus FROM RACUN WHERE IdRac=idRac");
+              int x=rs.getInt(1);
+              int y=rs.getInt(2);
+              if(x-sum>y)
+                      
+                  return true;
+              else return false;}
+              
+    }
+       void bm(int idRac) throws SQLException
+            {
+              try ( Connection conn = DriverManager.getConnection(connectionString);  Statement s = conn.createStatement()){
+              ResultSet rs=s.executeQuery("SELECT Stanje , DozvMinus FROM RACUN WHERE IdRac=idRac");
+              int x=rs.getInt(1);
+              int y=rs.getInt(2);
+              if(x>y)
+              {ResultSet rs1=s.executeQuery("UPDATE Racun SET STATUS 'A'"); 
+                  
+              }
+              
+              }              
+    }
+    
+    boolean zadatak(int idRacFrom, int idRacTo, float sum) throws SQLException 
+    {
+        try ( Connection conn = DriverManager.getConnection(connectionString);  Statement s = conn.createStatement()) {       String upit1="SELECT Stanje,DozvMinus FROM RACUN WHERE IdRac=idRacTo";
+        String upit="Update Racun Set Stanje=Stanje+? Where IdRac=?";
+       PreparedStatement ps=conn.prepareStatement(upit);
+       conn.setAutoCommit(false);
+       if(!(nesto(idRacFrom,sum))) return false;
+       ps.setDouble(1, sum);
+       ps.setInt(2, idRacFrom);
+       ps.executeUpdate();
+       PreparedStatement ps2=conn.prepareStatement(upit);
+       ps2.setDouble(1, -sum);
+       ps2.setInt(2,idRacTo);
+       ps2.execute();
+       bm(idRacTo);
+      
+       System.out.println("Uspesna realizacija");
+       conn.commit();
+       conn.setAutoCommit(true);
+       return true;
+         } catch (SQLException ex) {
+             System.out.println("Greska prilikom povezivanja na bazu");
+            System.out.println(ex);
+            return false;}
 
-}
+
+
+    }}
+
--- src/main/java/rs/edu/matgim/zadatak/Program.java
+++ src/main/java/rs/edu/matgim/zadatak/Program.java
@@ -1,11 +1,15 @@
 package rs.edu.matgim.zadatak;
 
+import java.sql.SQLException;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         DB _db = new DB();
-        _db.printFilijala();
+        _db.printPositiveRacun();
+        
+          bollean x=_db.zadatak(1, 2, (float) 1.4);
         
     }
 }
